20abf9a9fd7c174d375a046abd190df0810895fb
Parker Lawrence <liverspleenkidney@gmail.com>
2021-09-12 12:28:55 -0400

    procedural generation p2

--- Assets/Scripts/procedural.cs
+++ Assets/Scripts/procedural.cs
@@ -21,6 +21,8 @@ public class procedural : MonoBehaviour {
 
     private bool lb_last_building = false;
     private bool rb_last_building = false;
+    private bool lb_last_intersection = false;
+    private bool rb_last_intersection = false;
 
     private int leftside = -10;
     private int leftroad;
@@ -30,10 +32,11 @@ public class procedural : MonoBehaviour {
     private int rightroad;
     private int rightside;
     // private int 
-    void placeIntersectionAt(int x,int y) {
+    void placeIntersectionAt(int x,int y,bool leftclose,bool rightclose) {
         for (int xp = 0; xp <= 5; xp++) {
             for (int yp = 0; yp >= -5; yp--) {
-                tilemap.SetTile(new Vector3Int(xp+x, yp+y, 0),tileset.GetTile(new Vector3Int(xp-5, yp+5, 0)));
+                var dump = ((leftclose&&(xp==0||xp==1))||(rightclose&&(xp==4||xp==5)))&&((yp!=0 && yp!=-5)||(xp==0||xp==5));
+                tilemap.SetTile(new Vector3Int(xp+x, yp+y, 0),tileset.GetTile(new Vector3Int(xp-5, dump?6:yp+5, 0)));
             }
         }
     }
@@ -85,6 +88,22 @@ public class procedural : MonoBehaviour {
         var camext = new Vector2(camera.orthographicSize * Screen.width/Screen.height, camera.orthographicSize);
         var cammax = tilemap.WorldToCell((Vector2)camera.transform.position + camext);
         while (ybound<=cammax.y+3) {
+            if (lb_bound<=ybound && rb_bound<=ybound && l_road_bound==ybound && m_road_bound==ybound && r_road_bound==ybound && Random.Range(0, 2)==0 && !lb_last_intersection && !rb_last_intersection) {
+                l_road_bound+=6;
+                m_road_bound+=6;
+                r_road_bound+=6;
+                placeIntersectionAt(leftroad,l_road_bound,false,false);
+                placeIntersectionAt(middleroad,m_road_bound,false,false);
+                placeIntersectionAt(rightroad,r_road_bound,false,false);
+                lb_bound+=6;
+                rb_bound+=6;
+                placeEastRoadAt(buildings1,lb_bound);
+                placeEastRoadAt(buildings2,rb_bound);
+                lb_last_building=false;
+                rb_last_building=false;
+                lb_last_intersection=true;
+                rb_last_intersection=true;
+            }
             if (lb_bound<=ybound) {
                 while (true) {
                     var sentry = Random.Range(0,lb_last_building?4:3);
@@ -96,6 +115,7 @@ public class procedural : MonoBehaviour {
                         lb_bound+=5;
                         placeBuildingAt(buildings1,lb_bound,Random.Range(0, 3));
                         lb_last_building=true;
+                        lb_last_intersection=false;
                     } else if (sentry==1) {
                         if (lb_last_building) {
                             lb_bound+=1;
@@ -104,18 +124,23 @@ public class procedural : MonoBehaviour {
                         lb_bound+=3;
                         placeStationAt(buildings1,lb_bound,Random.Range(0, 3));
                         lb_last_building=true;
+                        lb_last_intersection=false;
                     } else if (sentry==2) {
-                        if (l_road_bound!=ybound || m_road_bound!=ybound) continue;
+                        if (l_road_bound!=ybound || m_road_bound!=ybound || lb_last_intersection) continue;
                         l_road_bound+=6;
                         m_road_bound+=6;
+                        placeIntersectionAt(leftroad,l_road_bound,false,false);
+                        placeIntersectionAt(middleroad,m_road_bound,false,true);
                         lb_bound+=6;
                         placeEastRoadAt(buildings1,lb_bound);
                         lb_last_building=false;
+                        lb_last_intersection=true;
                     } else if (sentry==3) {
                         var grasswidth = Random.Range(0, 3);
                         lb_bound+=2+grasswidth;
                         placeGrassAt(buildings1,lb_bound,grasswidth);
                         lb_last_building=false;
+                        lb_last_intersection=false;
                     }
                     break;
                 }
@@ -131,6 +156,7 @@ public class procedural : MonoBehaviour {
                         rb_bound+=5;
                         placeBuildingAt(buildings2,rb_bound,Random.Range(0, 3));
                         rb_last_building=true;
+                        rb_last_intersection=false;
                     } else if (sentry==1) {
                         if (rb_last_building) {
                             rb_bound+=1;
@@ -139,23 +165,27 @@ public class procedural : MonoBehaviour {
                         rb_bound+=3;
                         placeStationAt(buildings2,rb_bound,Random.Range(0, 3));
                         rb_last_building=true;
+                        rb_last_intersection=false;
                     } else if (sentry==2) {
-                        if (r_road_bound!=ybound || m_road_bound!=ybound) continue;
+                        if (r_road_bound!=ybound || m_road_bound!=ybound || rb_last_intersection) continue;
                         m_road_bound+=6;
                         r_road_bound+=6;
+                        placeIntersectionAt(middleroad,m_road_bound,true,false);
+                        placeIntersectionAt(rightroad,r_road_bound,false,false);
                         rb_bound+=6;
                         placeEastRoadAt(buildings2,rb_bound);
                         rb_last_building=false;
+                        rb_last_intersection=true;
                     } else if (sentry==3) {
                         var grasswidth = Random.Range(0, 3);
                         rb_bound+=2+grasswidth;
                         placeGrassAt(buildings2,rb_bound,grasswidth);
                         rb_last_building=false;
+                        rb_last_intersection=false;
                     }
                     break;
                 }
             }
-
             while (l_road_bound<=ybound) {
                 l_road_bound++;
                 placeNorthRoadAt(leftroad,l_road_bound,false);
